ea0bcdf5f328024206554eb64d9872de633fe6ef
Alan Macfarlane <alan.macfarlane@sciops.esa.int>
2016-06-03 16:27:14 +0200

    Initial commit

---
+++ src/esac/scrumcourse/Supermarket.java
@@ -0,0 +1,13 @@
+package esac.scrumcourse;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+
+public class Supermarket {
+	
+	public static void main(String[] args) {
+		Supermarket instance = new Supermarket();
+		
+	}
+}
---
+++ test/esac/scrumcourse/SuperMarketTest.java
@@ -0,0 +1,14 @@
+package esac.scrumcourse;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SuperMarketTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
