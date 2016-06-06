2dcaae8c3fc099465104f8651ff7e3bb26bf7dc2
Alan Macfarlane <alan.macfarlane@sciops.esa.int>
2016-06-06 18:20:43 +0200

    Correcting sonar bugs

--- src/esac/scrumcourse/Supermarket.java
+++ src/esac/scrumcourse/Supermarket.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 
 public class Supermarket {
 
+	private static final String END_PROGRAM = "end";
+	
 	static RegisterMachine registerMachine;
 
 	public static void main(String[] args) {
@@ -17,13 +19,16 @@ public class Supermarket {
 		try {
 			while (true) {
 				String productInput = InputHandler.read();
+				if(productInput.equalsIgnoreCase(END_PROGRAM)) {
+					break;
+				}
 				String currentTotalOutput = registerMachine.addProduct(productInput);
 
 				OutputHandler.write(currentTotalOutput);
 			}
 		} catch (IOException e) {
 
-			e.printStackTrace();
+			OutputHandler.write("There was an error with your input. The RegisterMachine will now end.");
 		}
 
 	}
